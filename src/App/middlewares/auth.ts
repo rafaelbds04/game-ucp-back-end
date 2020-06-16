7870bc60bbc26a5e8e578288597e720fdf3421f0
rafanel2 <38199191+rafanel2@users.noreply.github.com>
2020-06-16 13:30:26 -0300

    Some fix in auth and add user to request route

--- src/App/middlewares/auth.ts
+++ src/App/middlewares/auth.ts
@@ -1,32 +1,39 @@
-import { Request as defaultRequest, Response, NextFunction, RequestHandler, } from 'express';
+import { Request as defaultRequest, Response, NextFunction, RequestHandler, request, } from 'express';
 import { PrismaClient, userSelect } from '@prisma/client'
 import * as jwt from '../security/tokenParser';
 
-export interface Request extends defaultRequest {
-    user?: any
-  }
+export interface AuthRequest extends defaultRequest {
+    user?: {
+        id: number;
+        username: string;
+        admin: number;
+    }
+}
 
 const prisma = new PrismaClient();
 
 export enum Role {
     GUEST = -1,
     USER = 0,
+    MODERATOR = 4,
     ADMIN = 5
 }
 
 
 /**
  * This is equivalent to:
- * type RoleStrings = 'GUEST' | 'USER' | 'ADMIN';
+ * type RoleStrings = 'GUEST' | 'USER' | 'MODERATOR' | 'ADMIN';
  */
 type RoleStrings = keyof typeof Role;
 
 export const auth: (authorized: RoleStrings) => RequestHandler = (authorized) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
+    return async (req: AuthRequest, res: Response, next: NextFunction) => {
         const authorizedRole = Role[authorized];
         const authorization = req.headers.authorization;
 
+        //if this route is opened and the user isn't logged
         if (!authorization && authorizedRole === Role.GUEST) return next();
+        //if this route is private and the user isn't send the authorization header
         if (!authorization) return res.status(401).send({ error: { message: 'No authorization header set' } })
 
         try {
@@ -38,9 +45,13 @@ export const auth: (authorized: RoleStrings) => RequestHandler = (authorized) =>
                 select: { id: true, username: true, admin: true }
             })
 
-            if(authorizedRole !== result?.admin) { return res.status(401).send({ error: { message: `Permission denied. Required rule: ${authorized}` }}) }
+            if (!result) { return res.status(401).send({ error: { message: `User not find` } }) }
 
-            req.user = result;
+            const { id, username, admin } = result!;
+
+            if (!(admin! >= authorizedRole)) { return res.status(401).send({ error: { message: `Permission denied. Required role: ${authorized}` } }) }
+
+            req.user = { id, username, admin };
 
             next();
         } catch (error) {
